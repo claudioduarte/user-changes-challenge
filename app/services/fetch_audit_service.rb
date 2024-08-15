0b42f3161631c0a7f0c7f3e2b4cbf764bb06679d
Cláudio Duarte <hello@claudioduarte.pt>
2024-08-15 17:34:27 +0100

    safe operator when audits missing

--- app/services/fetch_audit_service.rb
+++ app/services/fetch_audit_service.rb
@@ -17,16 +17,16 @@ class FetchAuditService
     newest_change = nil
     result = []
 
-    @audit.each_with_index do |k, v|
+    @audit&.each_with_index do |k, v|
       audit_changes << k['audit_changes']
     end
 
-    audit_changes.group_by { |ac| ac['_id'] }.each do |c|
+    audit_changes.group_by { |ac| ac['_id'] }&.each do |c|
       oldest_change = c.second.to_a.first
       newest_change = c.second.to_a.last
     end
 
-    oldest_change.each do |k, v|
+    oldest_change&.each do |k, v|
       result << { field: k, old: oldest_change[k], new: newest_change[k] }
     end
 
